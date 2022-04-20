d8eafdfa5439efbed7604116a12205028491ebe2
nreiscaio <nreis.caio@gmail.com>
2022-04-20 14:06:37 -0300

    Aula Senac

--- Atividades/Atividade01/js/script.js
+++ Atividades/Atividade01/js/script.js
@@ -5,9 +5,9 @@ let altura = '1.70'
 let cpf = '12345678910'
 let rg = '12345678'
 
-document.write(`Nome: ${nome}`)
-document.write(`Idade: ${idade}`)
-document.write(`Peso: ${peso}`)
-document.write(`Altura: ${alrura}`)
-document.write(`CPF: ${cpf}`)
-document.write(`RG:: ${rg}`)
+document.write(`Nome: ${nome} `)
+document.write(`Idade: ${idade} `)
+document.write(`Peso: ${peso} `)
+document.write(`Altura: ${altura} `)
+document.write(`CPF: ${cpf} `)
+document.write(`RG: ${rg} `)
